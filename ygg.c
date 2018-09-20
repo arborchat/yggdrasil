03cd032cd98d835f4e05af8624d9a8f0941728e3
Christopher Waldon <christopher.waldon.dev@gmail.com>
2018-09-20 18:08:03 -0400

    Read a message from the server

--- ygg.c
+++ ygg.c
@@ -1,3 +1,12 @@
+// for malloc
+#include <stdlib.h>
+
+// for read
+#include <unistd.h>
+
+// for memset
+#include <string.h>
+
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -5,6 +14,27 @@
 
 // for htons and friends
 #include <arpa/inet.h>
+
+#define READ_BUF_SIZE 1024
+
+// read_line reads a newline-delimited string of text from the given file descriptor
+// It returns a pointer to the null-terminated string containing the text, and sets the
+// value of bytes_read to indicate the length of the string that it returned.
+// The returned string is dynamically allocated and must be `free()`ed.
+char *read_line(int fd, int *bytes_read) {
+    char * read_buf = (char *) malloc(READ_BUF_SIZE * sizeof(char));
+    memset(read_buf, 0, READ_BUF_SIZE);
+    int index = 0;
+    int num_read = read(fd, read_buf, READ_BUF_SIZE);
+    for (; index <= num_read; index++) {
+	if (read_buf[index] == '\n') {
+    		read_buf[index] = '\0';
+    		*bytes_read = index + 1;
+    		return read_buf;
+	}
+    }
+    return "";
+}
 
 int main(int argc, char* argv[]) {
     int tcp_sock;
@@ -31,9 +61,6 @@ int main(int argc, char* argv[]) {
     printf("Connection successful\n");
 
     // communicate
-    char c[1] = {0};
-    if (read(tcp_sock, c, 1) < 1) {
-        perror("Failed to read socket");
-    }
-    printf("%s\n", c);
+    int num_read;
+    printf("%s\n", read_line(tcp_sock, &num_read));
 }
