d317daf4733ca6588f0b0b9f59b1e0a5c23f340b
Christopher Waldon <christopher.waldon.dev@gmail.com>
2018-09-24 21:30:09 -0400

    Use poll to monitor socket and stdin

--- ygg.c
+++ ygg.c
@@ -4,6 +4,12 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+// for STDIN_FILENO
+#include <unistd.h>
+
+// for poll
+#include <poll.h>
+
 // for strlen
 #include <string.h>
 
@@ -39,16 +45,27 @@ int main(int argc, char* argv[]) {
     }
     printf("Connection successful\n");
 
+    // set up file monitoring
+    struct pollfd fds[2];
+    memset(&fds, 0, sizeof(struct pollfd)*2);
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[1].fd = tcp_sock;
+    fds[1].events = POLLIN;
+
     // communicate
     FILE *sockfile = fdopen(tcp_sock, "rw");
     arbor_msg_t message;
-    while (read_arbor_message(sockfile, &message)) {
-        if (message.type == ARBOR_WELCOME) {
-            printf("Type: %d Major: %d Minor: %d Root: %s Recent_Len: %d\n", message.type, message.major, message.minor, message.root, (int) message.recent_len);
-        } else if (message.type == ARBOR_NEW) {
-            printf("[%s]@%d %s: %s", message.uuid, message.timestamp, message.username, message.content);
-            if (message.content[strlen(message.content) -1] != '\n') {
-                printf("\n");
+    while (poll(fds, 2, -1)) {
+        if (fds[1].revents & POLLIN) {
+            read_arbor_message(sockfile, &message);
+            if (message.type == ARBOR_WELCOME) {
+                printf("Type: %d Major: %d Minor: %d Root: %s Recent_Len: %d\n", message.type, message.major, message.minor, message.root, (int) message.recent_len);
+            } else if (message.type == ARBOR_NEW) {
+                printf("[%s]@%d %s: %s", message.uuid, message.timestamp, message.username, message.content);
+                if (message.content[strlen(message.content) -1] != '\n') {
+                    printf("\n");
+                }
             }
         }
     }
